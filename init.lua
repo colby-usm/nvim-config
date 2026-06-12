3f102fa31b2dd874d5211c7d46542c7f69f598f7
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2026-06-12 16:14:10 -0400

    swapped harpoon for harpoon-core

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- See `:help vim.o`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
@@ -7,7 +6,7 @@ require 'options'
 require 'keymaps'
 require 'autocmds'
 
--- [[ Install `lazy.nvim` plugin manager ]]
+-- [[ Install lazy.nvim ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -16,80 +15,30 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     error('Error cloning lazy.nvim:\n' .. out)
   end
 end
+vim.opt.rtp:prepend(lazypath)
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
---    :Lazy
---    :Lazy update
--- NOTE: Here is where you install your plugins.
 require('lazy').setup {
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  { 'NMAC427/guess-indent.nvim' }, -- Detect tabstop and shiftwidth automatically
-
+  { 'NMAC427/guess-indent.nvim' },
   {
-    'neovim/nvim-lspconfig',
-  },
-
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
     ft = 'lua',
     opts = {
       library = {
-        -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
   },
-
   {
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+        styles = { comments = { italic = false } },
       }
       vim.cmd.colorscheme 'default'
     end,
   },
-
-  -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  },
   { import = 'kickstart.plugins' },
   { import = 'custom.plugins' },
 }
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,39 +1,43 @@
 return {
-  'ThePrimeagen/harpoon',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local mark = require 'harpoon.mark'
-    local ui = require 'harpoon.ui'
+  {
+    'MeanderingProgrammer/harpoon-core.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    build = function()
+      local file = vim.fn.stdpath 'data' .. '/lazy/harpoon-core.nvim/lua/harpoon-core/ui.lua'
+      local content = table.concat(vim.fn.readfile(file), '\n')
+      content = content:gsub("vim.api.nvim_create_autocmd%('BufModifiedSet'", "vim.api.nvim_create_autocmd('OptionSet', { pattern = 'modified' } --[[")
+      vim.fn.writefile(vim.split(content, '\n'), file)
+    end,
+    config = function()
+      local harpoon = require 'harpoon-core'
+      harpoon.setup()
 
-    -- Keymaps for Harpoon
-    -- Add a file to Harpoon
-    vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Add file to Harpoon' })
-    -- Toggle Harpoon menu
-    vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu, { desc = 'Toggle Harpoon menu' })
-    -- Go to Harpoon files 1-7
-    vim.keymap.set('n', '<leader>1', function()
-      ui.nav_file(1)
-    end, { desc = 'Harpoon file 1' })
-    vim.keymap.set('n', '<leader>2', function()
-      ui.nav_file(2)
-    end, { desc = 'Harpoon file 2' })
-    vim.keymap.set('n', '<leader>3', function()
-      ui.nav_file(3)
-    end, { desc = 'Harpoon file 3' })
-    vim.keymap.set('n', '<leader>4', function()
-      ui.nav_file(4)
-    end, { desc = 'Harpoon file 4' })
-    vim.keymap.set('n', '<leader>5', function()
-      ui.nav_file(4)
-    end, { desc = 'Harpoon file 5' })
-    vim.keymap.set('n', '<leader>6', function()
-      ui.nav_file(4)
-    end, { desc = 'Harpoon file 6' })
-    vim.keymap.set('n', '<leader>7', function()
-      ui.nav_file(4)
-    end, { desc = 'Harpoon file 7' })
-    vim.keymap.set('n', '<leader>8', function()
-      ui.nav_file(4)
-    end, { desc = 'Harpoon file 8' })
-  end,
+      vim.keymap.set('n', '<leader>a', harpoon.add_file, { desc = 'Add file to Harpoon' })
+      vim.keymap.set('n', '<leader>h', harpoon.toggle_quick_menu, { desc = 'Toggle Harpoon menu' })
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon.nav_file(1)
+      end, { desc = 'Harpoon file 1' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon.nav_file(2)
+      end, { desc = 'Harpoon file 2' })
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon.nav_file(3)
+      end, { desc = 'Harpoon file 3' })
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon.nav_file(4)
+      end, { desc = 'Harpoon file 4' })
+      vim.keymap.set('n', '<leader>5', function()
+        harpoon.nav_file(5)
+      end, { desc = 'Harpoon file 5' })
+      vim.keymap.set('n', '<leader>6', function()
+        harpoon.nav_file(6)
+      end, { desc = 'Harpoon file 6' })
+      vim.keymap.set('n', '<leader>7', function()
+        harpoon.nav_file(7)
+      end, { desc = 'Harpoon file 7' })
+      vim.keymap.set('n', '<leader>8', function()
+        harpoon.nav_file(8)
+      end, { desc = 'Harpoon file 8' })
+    end,
+  },
 }
